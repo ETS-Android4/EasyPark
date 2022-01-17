7ae70c300e42e0fdea24d69fe80cad3f4ee5aa63
Feras Awaga <feras@aucegypt.edu>
2022-01-17 15:27:06 +0200

    Initial Commit

---
+++ AUCPark/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AUCPark
+//
+//  Created by Feras Awaga on 1/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
